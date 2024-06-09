7c56808dd6ae0cd158174ea7f4922e45c78979c3
saipan-fez <saipan-fez@users.noreply.github.com>
2024-06-10 02:45:10 +0900

    fix section algorithm

---
+++ GenshinTimeSplitter/Extensions/VideoCaptureExtension.cs
@@ -0,0 +1,27 @@
+﻿using OpenCvSharp;
+using System;
+
+namespace GenshinTimeSplitter.Extensions;
+
+public static class VideoCaptureExtension
+{
+    // !!! ATTENTION !!!
+    // In the VideoCapture class, the values of PosMsec and FrameCount can be inaccurate.
+    // So we will calculate it originally from VideoCapture.PosFrames.
+    // ref: https://github.com/opencv/opencv/issues/15749
+    // ref: https://stackoverflow.com/a/41684390
+
+    public static TimeSpan GetPosTimeSpan(this VideoCapture videoCapture)
+    {
+        var currentFramePos = videoCapture.PosFrames;
+        var fps = videoCapture.Fps;
+        return TimeSpan.FromSeconds(currentFramePos / fps);
+    }
+
+    public static void Seek(this VideoCapture videoCapture, TimeSpan timeSpan)
+    {
+        var fps = videoCapture.Fps;
+        var seekFrame = (int)Math.Round(fps * timeSpan.TotalSeconds);
+        videoCapture.PosFrames = seekFrame;
+    }
+}
